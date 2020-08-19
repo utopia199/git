2aba243c076ee844f1eae0956fe1e0dbbe2fcb68
utopia199 <676096691@qq.com>
2020-08-19 18:09:45 +0800

    判断model
    判断model

--- nodes/api/temp.js
+++ nodes/api/temp.js
@@ -7,49 +7,57 @@ exports.getTemp = function (req,res){// 获取模板
     let obj = new Object();
     let body = req.body;
     let filesObj = [];
-     
-    async function temp() {
-       await GetFile(body.href).then((files,reject)=>{
-            for(let i = 0 ; i < files.length; i++) {
-                if(!files[i].includes('.')){
-                //     GetFile(body.href + "\\" + files[i]).then(temps=>{
-                        let nodemodules = false;
-                //         if(temps){
-                //             nodemodules = true
-                //         }
-                //         filesObj.push({
-                //             file: files[i],
-                //             nodemodules: nodemodules
-                //         })
-                //     }).catch(()=>{})
-                    filesObj.push({
-                        file: files[i],
-                        nodemodules: nodemodules
-                    })
+    GetTemp()
+    function GetTemp() {
+        let len = 1,
+            i = 0,
+            ll = [];
+        GetFile(body.href).then(files=>{
+            for(let index = 0 ; index < files.length; index++) {
+                if(!files[index].includes('.')){
+                    ll.push(files[index])
                 }
-               
             }
-            obj.status_code = 200;
-            obj.item = filesObj;
-            res.json(obj);
+            len = ll.length
+            pushList(body.href + "\\" + ll[i],type=>{
+                filesObj.push({
+                    file: ll[i],
+                    nodemodules: type
+                })
+            })
         }).catch(err=>{
             obj.status_code = 400;
             obj.message = "路径有误";
             res.json(obj);
         })
+        function pushList(href,callback) {
+            GetFile(body.href + "\\" + ll[i]).then(files=>{
+                if( i < len-1) {
+                   callback( files.includes("node_modules"))
+                    i++
+                   pushList(body.href + "\\" + ll[i],callback)
+                } else {
+                    obj.status_code = 200;
+                    obj.item = filesObj;
+                    res.json(obj);
+                }
+            }).catch(()=>{
+                obj.status_code = 400;
+                obj.message = "路径有误";
+                res.json(obj);
+            })
+        }
     }
-    temp()
 }
 exports.install = function (req,res){// 初始化下载node
     let body = req.body;
     let obj = new Object();
-    if( body.temp ){
+    if( typeof body.temp === "string"){
         console.log("开始初始化-----",CodePath + body.temp)
         const serve = spawn('npm install',{
             cwd:CodePath + body.temp,
             shell: true
         })
-
         serve.once('close', function () {
             console.log("初始化完成-----",CodePath + body.temp)
             obj.status_code = 200;
@@ -61,7 +69,6 @@ exports.install = function (req,res){// 初始化下载node
         obj.message =  "参数有误";
         res.json(obj);
     }
-    
 }
 exports.build = function (req,res){// 打包模板
     let body = req.body;
@@ -71,27 +78,23 @@ exports.build = function (req,res){// 打包模板
             cwd:CodePath + body.temp,
             shell: true
         });
-        
-
-
-        
-
-       
-        
-       
         // 执行完成触发
         serve.once('close', function () {
             console.log("打包完成-----执行拷贝",CodePath + body.temp)
-            
             let Desktop = require('path').join(require('os').homedir(), 'Desktop') + "\\dist"; // 桌面路径
-            fs.mkdirSync(Desktop);//创建dist文件夹
-            copyFolder(CodePath + body.temp + "\\temp",Desktop).then(()=>{
-                obj.status_code = 200;
-                obj.message =  "已在桌面生成dist文件夹";
-                res.json(obj);
+            GetFile(require('path').join(require('os').homedir(), 'Desktop')).then(desktopF=>{
+                if(!desktopF.includes("dist")){
+                    fs.mkdirSync(Desktop);//创建dist文件夹
+                }
+                copyFolder(CodePath + body.temp + "\\temp",Desktop).then(()=>{
+                    obj.status_code = 200;
+                    obj.message =  "已在桌面生成dist文件夹";
+                    res.json(obj);
+                    console.log("拷贝成功",CodePath + body.temp)
+                }).catch(()=>{})
             })
+            
         })
-
         function copyFolder(from, to){ // 复制文件夹到指定目录
             return new Promise((resolve,reject)=>{
                 let files = [];
@@ -99,20 +102,20 @@ exports.build = function (req,res){// 打包模板
             
                 files.forEach(file=> {// 遍历temp 文件夹
 
-                    let targetPath = from + "/" + file;
-                    let toPath = to + '/' + file;
+                    let targetPath = from + "\\" + file;
+                    let toPath = to + "\\" + file;
                     if (fs.statSync(targetPath).isDirectory()) {// 判断temp下是否是文件夹
 
                         fs.mkdirSync(toPath);// 新建文件夹
                         fs.readdirSync(targetPath).forEach(data=>{// 遍历temp文件夹下面的文件夹
-                            let tempFile = targetPath+"/"+ data;
-                            let DesktopFile = toPath+"/"+ data;
+                            let tempFile = targetPath+"\\"+ data;
+                            let DesktopFile = toPath+"\\"+ data;
                             if (fs.statSync(tempFile).isDirectory()) {// 判断temp下是否是文件夹
 
                                 fs.mkdirSync(DesktopFile);// 新建temp下文件夹 的 文件夹
                                 fs.readdirSync(tempFile).forEach(children=>{// 遍历temp文件夹下面的文件夹
-                                    let tempFile1 = tempFile+"/"+ children;
-                                    let DesktopFile1 = DesktopFile+"/"+ children;
+                                    let tempFile1 = tempFile + "\\" +  children;
+                                    let DesktopFile1 = DesktopFile + "\\" + children;
                                     fs.copyFileSync(tempFile1, DesktopFile1);
                                 })
                                 
