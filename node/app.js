6e0563cec16e44a1e60e5d3edd237fa2e878741f
utopia199 <676096691@qq.com>
2020-08-21 14:28:52 +0800

    1
    1

--- node/app.js
+++ node/app.js
@@ -24,6 +24,15 @@ const fs = require("fs");
 //     console.log(result);
 //     res.send('Database created...');
 // })
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({ port: 8181 });//服务端口8181
+wss.on('connection', function (ws) {
+    console.log('服务端：客户端已连接');
+    ws.on('message', function (message) {
+        //打印客户端监听的消息
+        console.log(message);
+    });
+});
 
 // 原本应该是操作数据库的方法
 global.GET_FILE_CONTENT = function (file){// 读取文件内容
