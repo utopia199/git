95754a9e897921efe9d7850725b9f59cfa25660b
utopia199 <676096691@qq.com>
2020-09-05 16:29:03 +0800

    1
    1

--- node/app.js
+++ node/app.js
@@ -2,28 +2,8 @@ const express = require("express");
 const app = require('./router/index.js');
 const path = require("path");
 const fs = require("fs");
-// const querystring = require("querystring");
 // npm install -g supervisor  解决修改代码后需要重新运行   supervisor app.js
 
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost', // 主机名
-//   user     : 'root',// 用户名
-//   password : '123456b',// 密码
-// //   database : 'demo'// 数据库名称
-// });
- 
-// connection.connect();// 创建连接
- 
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-// connection.query("CREATE DATABASE demo", (err, result) => {
-//     if(err) throw err;
-//     console.log(result);
-//     res.send('Database created...');
-// })
 
 
 // 原本应该是操作数据库的方法
