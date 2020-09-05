190d7686d97c6a4bc3910b2b6435475662bf488b
utopia199 <676096691@qq.com>
2020-09-05 18:24:16 +0800

    1
    1

--- node/app.js
+++ node/app.js
@@ -4,13 +4,19 @@ const path = require("path");
 const fs = require("fs");
 // npm install -g supervisor  解决修改代码后需要重新运行   supervisor app.js
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/node";
+var url = "mongodb://127.0.0.1:27017/";
  
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-  if (err) throw err;
-  console.log("数据库已创建!");
-  db.close();
-});
+// MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+//   console.log("数据库已创建!");
+//   db.close();
+   
+// });
+dbo.collection("node"). find({}).toArray(function(err, result) { // 返回集合中所有数据
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
 
 
 // 原本应该是操作数据库的方法
