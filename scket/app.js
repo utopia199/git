5632ec56ac9bbc402759eab06d05dca42c197055
utopia199 <676096691@qq.com>
2020-09-17 17:43:59 +0800

    1
    1

--- scket/app.js
+++ scket/app.js
@@ -10,19 +10,19 @@ server.listen(4949, function() {
     console.log('Server listening at http://10.0.6.103:4949/');
 });
 io = sio.listen(server);
-var messages = [];
 io.sockets.on('connection', function(socket) {
     socket.on('message', function(msg) {
         var clientIp = socket.request.connection.remoteAddress
-        messages.push({ip: clientIp,message: msg});
-        socket.broadcast.emit('message', {ip: clientIp,message: msg});
-        socket.send({ip: clientIp,message: msg});
+        let datas = {ip: clientIp,message: msg}
+        socket.broadcast.emit('message', datas);
+        mongo(datas)
+        socket.send(datas);
     });
- 
-    messages.forEach(function(msg) {
-        socket.send(msg);
+    getmessage(me=>{
+        me.forEach(function(msg) {
+            socket.send(msg);
+        })
     })
-
 });
 function mongo(data) {
     const MongoClient = require('mongodb').MongoClient;
@@ -33,5 +33,20 @@ function mongo(data) {
         dbs.collection("message").insertOne(data, function(err, resolve) {// 插入消息
             db.close();
         })
+    })
+}
+function getmessage(call) {
+    const MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb://127.0.0.1:27017/";
+    MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+        if(err) throw err
+        let dbs = db.db("node");
+
+        dbs.collection("message").find().toArray((err,result)=>{
+			if(err ) throw err
+            call(result)
+			db.close();
+			
+		})
     })
 }
